47aa6f6c935056b07f2bbc09182c9f09eb420061
Barbara Candido <barbara.candido@unifesp.br>
2021-06-15 18:04:59 -0300

    Exercícios 3.4 e 3.5, aula 01

---
+++ src/main/java/cotuba/Main.java
@@ -0,0 +1,49 @@
+package cotuba;
+
+import java.nio.file.Path;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+	LeitorOpcoesCLI opcoesCLI = new LeitorOpcoesCLI(args);
+	
+	
+	Path diretorioDosMD = opcoesCLI.getDiretorioDosMD();
+	
+	
+	String formato = opcoesCLI.getFormato();
+	
+	
+	Path arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
+	
+	
+	boolean modoVerboso = opcoesCLI.isModoVerboso();
+	
+
+		try {
+
+						
+			if ("pdf".equals(formato)) {
+				GeradorPDF geradorPDF = new GeradorPDF();
+				geradorPDF.gera(diretorioDosMD, arquivoDeSaida);
+
+			} else if ("epub".equals(formato)) {
+				GeradorEPUB geradorEPUB = new GeradorEPUB();
+				geradorEPUB.gera(diretorioDosMD, arquivoDeSaida);
+			} else {
+				throw new RuntimeException("Formato do ebook inválido: " + formato);
+			}
+
+			System.out.println("Arquivo gerado com sucesso: " + arquivoDeSaida);
+
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+			if (modoVerboso) {
+				ex.printStackTrace();
+			}
+			System.exit(1);
+		}
+	
+	}
+}
